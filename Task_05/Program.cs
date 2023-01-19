b2b7ea362e64b1c333212f9ea6a7b315d7093638
Liudmila <topal.liudmila89@gmail.com>
2023-01-19 22:15:56 +0200

    Task 05

--- Task_05/Program.cs
+++ Task_05/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n * (-1);
+while (m <= n)
+{
+    Console.Write($"{m} ");
+    m++;
+}
