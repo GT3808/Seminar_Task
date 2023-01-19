e8615d4350a00c2b45bc404c149c0ccdf984f8e6
Liudmila <topal.liudmila89@gmail.com>
2023-01-19 18:02:40 +0200

    Doc

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+Console.Clear(); // очистка консоли
 Console.WriteLine("Hello, World!");
+*/
+// ctrl + /
+// Console.Clear();
+// int a = 1, b = 2;
+// Console.WriteLine($"{a}, {b}");
+// int n = int.Pasre(Console.ReadLine());
+// Console.WriteLine($"Result: {n * 3}");
+// Result: 15
+
+// Console.Clear();
+// Console.Write("Enter 1-st number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter 2-st number: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine("=");
+
+
+
+
