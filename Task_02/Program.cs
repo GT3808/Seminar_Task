b11d7b14f0f872fa315bdf780efbf6ab7ca91fd0
Liudmila <topal.liudmila89@gmail.com>
2023-01-19 22:50:55 +0200

    Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter 1-st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2-st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine(n);
+else
+    Console.WriteLine(m);
