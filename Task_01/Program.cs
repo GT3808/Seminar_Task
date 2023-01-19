393374d81d103e2076b242968157bde7fa0c531f
Liudmila <topal.liudmila89@gmail.com>
2023-01-19 18:22:05 +0200

    Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+ int a = 4, b = -3;
+Console.WriteLine($"{a}, {b}");
+ int n = int.Pasre(Console.ReadLine());
+ Console.WriteLine($"Result: {n * 2}");
+ 
