1ac8ee35bb5f87872447580ca450e696050e726b
Liudmila <topal.liudmila89@gmail.com>
2023-01-19 22:02:19 +0200

    Task 03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Enter the day of the week: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// && - и(логические умножение)
+// || -  или (логиское сложение)
+// True - 1
+// False - 0
+//       1    +   0   = 1(True)
+while (n < 1 || n > 7)
+{
+    Console.Write("You are mistaken!\nEnter the day of the week: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+    Console.WriteLine("Monday");
+else if (n == 2)
+    Console.WriteLine("Tuesday");
+else if (n == 3)
+    Console.WriteLine("Wednesday");
+else if (n == 4)
+    Console.WriteLine("Thursday");
+else if (n == 5)
+    Console.WriteLine("Friday");
+else if (n == 6)
+    Console.WriteLine("Saturday");
+else
+    Console.WriteLine("Sunday");
+
