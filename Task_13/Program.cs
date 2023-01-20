1a42908022327de606badec45f4623c2f9951424
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 18:18:01 +0200

    Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("No third digit");
+else
+{
+    while (n >= 999)
+    {
+          n = n / 10;
+    }
+    Console.WriteLine(n);
+    Console.WriteLine(n % 10);
+}
