ee267771996bfbd627cfa3eea047c620f8a560e3
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 13:52:45 +0200

    Task Demo

---
+++ Task_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine($"Random number: {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1 > n2)
+    Console.Write(n1);
+else
+    Console.Write(n2);
+
