2bcfb1ad623f36e649fd8e76010316d6ee21c418
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 12:23:38 +0200

    Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 1)
+    Console.WriteLine("Odd");
+else
+    Console.WriteLine("Even");
