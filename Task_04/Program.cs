8dd76025389081f17a7370be10315c799d4d7b02
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 10:20:15 +0200

    Task 04

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,3 +1,13 @@
 ﻿Console.Clear();
-Console.Write("Enter 1-st number");
+Console.Write("Enter 1-st number: ");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2-st number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 3-st number: ");
+int c = Convert.ToInt32(Console.ReadLine()), max = a;
+if (max < b)
+    max = b;
+if (max < c)
+    max = c;
+Console.WriteLine(max);
+
