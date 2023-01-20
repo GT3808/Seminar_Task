ef745d76100f4dedd64f63ee1797d4989da95db5
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 12:58:42 +0200

    Task 08

---
+++ Task_08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 2;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
