852f804b4b5781397cbc1d28e5d25b8a05f6049d
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 16:33:02 +0200

    Task 16

---
+++ Task_16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter 1-st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2-st number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m || m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
