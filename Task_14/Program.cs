7ba726cf10a1820a1a256dbecff9cfe5423f755c
Liudmila <topal.liudmila89@gmail.com>
2023-01-20 16:14:19 +0200

    Task 14

---
+++ Task_14/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//if (n % 7 == 0 && n % 23 == 0)
+if (n % 161 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
