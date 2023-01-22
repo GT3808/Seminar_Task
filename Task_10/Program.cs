a2827bf65d5c98019e96d72051ed04c256d779e0
Liudmila <topal.liudmila89@gmail.com>
2023-01-22 09:28:59 +0200

    Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task 10
+
+Console.Clear();
+Console.Write("Three digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10;
+int n2 = n1 % 10;
+Console.WriteLine($"{n2}");
