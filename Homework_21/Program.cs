5a5eb4cc92dcd986db7fb124164581732c5c45f4
Liudmila <topal.liudmila89@gmail.com>
2023-01-27 18:51:45 +0200

    Homework 21 modified

--- Homework_21/Program.cs
+++ Homework_21/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Homework 21
 
+/*
 Console.Clear();
 Console.Write("Enter X1: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
@@ -15,3 +16,20 @@ Console.Write("Enter Y3: ");
 double y3 = Convert.ToDouble(Console.ReadLine());
 double m = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)+ Math.Pow(x3 - y2, 2)), 2);  // число с остатком
 Console.WriteLine(m);
+*/
+
+Console.Clear();
+Console.Write("Enter X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double m = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+ Math.Pow(z1 - z2, 2));  // число с остатком
+Console.WriteLine($"Result: {Math.Round(m, 2)}");
