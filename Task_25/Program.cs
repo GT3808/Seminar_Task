0fcc597362c8e9b69e65cfa608f474c04928066d
Liudmila <topal.liudmila89@gmail.com>
2023-01-27 09:39:04 +0200

    Task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 25
+
+Console.Clear();
+Console.Write("Enter 1-st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2-st number: ");
+int m = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i<= m; i++)
+     result = result * n; // result *= n;
+Console.WriteLine(result);
+
