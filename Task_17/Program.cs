6702edecad4920acbecbc74c4798b5f26ac5700c
Liudmila <topal.liudmila89@gmail.com>
2023-01-27 14:28:36 +0200

    Task 17

---
+++ Task_17/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 17
+
+Console.Clear();
+Console.Write("Enter coordinate X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter coordinate Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");             
+
