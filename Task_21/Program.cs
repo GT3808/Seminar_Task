dd1a511e90dd4437c0cc03c0096714c6394341e1
Liudmila <topal.liudmila89@gmail.com>
2023-01-27 17:12:28 +0200

    Task 21

---
+++ Task_21/Program.cs
@@ -0,0 +1,47 @@
+﻿// Task 21
+
+/*Console.Clear();
+Console.Write("Enter X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Pow(5, 2) */
+
+/* Console.Clear();
+Console.Write("Enter X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)); */
+
+/* 
+Console.Clear();
+Console.Write("Enter X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2))); */ // до целого числа
+
+Console.Clear();
+Console.Write("Enter X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)), 2);  // число с остатком
+Console.WriteLine(l);
