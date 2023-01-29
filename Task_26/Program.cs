162b14e014f9b62e1569fca37c8a326b51e2b301
Liudmila <topal.liudmila89@gmail.com>
2023-01-29 21:04:53 +0200

    Task 26

---
+++ Task_26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 26
+
+// Напишите програму, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+// 456 - 3; 78 - 2; 89126 - 5;
+
+Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (n > 0)
+{
+      n = n / 10;
+      count++;
+}
+Console.WriteLine(count);
