580b47c14f902d244322ba2b01518012b98d5739
Liudmila <topal.liudmila89@gmail.com>
2023-01-29 20:47:28 +0200

    Task 24

---
+++ Task_24/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 24
+
+// Напишите программу, которая принимает на вход число (A)
+// и выдает сумму чисел от 1 до A.
+
+// 7 - 28; 4 - 10; 8 - 36; 
+
+Console.Clear();
+Console.Write("Enter number: ");
+double n = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine($"Result: {(1 + n) / 2 * n}");
