763f5000635b1c8cac3d5745f74b763046fded46
Liudmila <topal.liudmila89@gmail.com>
2023-01-29 20:32:33 +0200

    Function Void modified

--- Function_Void/Program.cs
+++ Function_Void/Program.cs
@@ -30,6 +30,8 @@ Console.WriteLine(n);
 
 */
 
+/*
+
 Console.Clear();
 void f(int[] n)
 { //          1
@@ -38,3 +40,45 @@ void f(int[] n)
 int[] n = {1, 2, 3, 4, 5};
 f (n);
 Console.WriteLine(string.Join(" ", n));
+
+*/
+
+/*
+
+Напишите программу, которая принимает 
+2 числа и выводит наибольшее
+
+
+
+int f(int a, int b)
+{
+    if (a > b)
+       return a;  // Функция завершит свою работу //
+     return b;
+}
+
+int a = 10, b = 7;
+Console.WriteLine(f(a, b));
+
+*/
+
+/*
+
+int f(int a, int b)
+{
+    if (a > b)
+       return a;
+     return b;
+}
+
+int a = 10, b = 7;
+int result = f(a, b);
+Console.WriteLine(result);
+
+///  Кааой тип данных нам возврощаются в такую переменную
+     мы должны принимать результат, либо 
+      сразу выводить на экран                              ///
+                                
+
+*/
+
