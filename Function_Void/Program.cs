a8b8af9e8cb9eb856c5da03edbd8604fb09d01b3
Liudmila <topal.liudmila89@gmail.com>
2023-01-29 19:22:35 +0200

    Function Void

---
+++ Function_Void/Program.cs
@@ -0,0 +1,28 @@
+﻿// Function void
+
+/*
+
+void f (int n)
+{
+  n = n + 5;
+}
+
+int n = 10;
+f (n);
+Console.WriteLine (n);
+
+//  передача данных не выполнено  //
+
+*/
+
+Console.Clear();
+void f (ref int n)
+{
+     n = n + 5;
+}
+
+int n = 10;
+f (ref n);
+Console.WriteLine(n);
+
+// ref - передача значение по ссылке //
