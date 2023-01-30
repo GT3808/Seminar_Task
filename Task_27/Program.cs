343ac95d3c81cb27924c22f39712b9ddd12adbaa
Liudmila <topal.liudmila89@gmail.com>
2023-01-30 13:37:47 +0200

    Task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 27
+
+// Напишите програму, которая принимает на вход число 
+// выдаёт сумму цифр в числе.
+
+// нахождение в сумме цифр в любом числе
+
+Console.Clear();
+Console.Write("Enter number:");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (n > 0)
+{
+      result = result + n % 10;     // result += n % 10;
+      n = n / 10;                  // n /= 10;
+}
+Console.WriteLine(result);         // adunam cifrele intre ele +- //
