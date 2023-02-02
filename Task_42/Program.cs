be00c0398e46e3d4cda2399a93b69651b42d2798
Liudmila <topal.liudmila89@gmail.com>
2023-02-02 20:50:43 +0200

    Task 42

---
+++ Task_42/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 42 
+
+
+/*    Напишите программу, которая будет преобразовывать
+      десятичное число в двоичное.
+
+      45 -> 101101
+       3  -> 11
+       2  -> 10                                  */
+
+
+Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n > 0)
+{
+    result = Convert.ToString(n % 2) + result;
+     n = n / 2;
+}
+Console.WriteLine(result);
