b561b9e0ffce5dad9f1865f0bb60c9b5f87556b0
Liudmila <topal.liudmila89@gmail.com>
2023-02-08 11:03:28 +0200

    Modified Task 47

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -10,7 +10,7 @@
       8 7,8 -7,1 9         */
 
 
-Console.Write("Number of Rows Two-Dimensional Array: ");
+/*  Console.Write("Number of Rows Two-Dimensional Array: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Number of Columns Two-Dimensional Array: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -41,4 +41,25 @@ void FillArray(double[,] matr)
 
 FillArray(TwoDimensArray);
 Console.WriteLine();
-PrintArray(TwoDimensArray);
+PrintArray(TwoDimensArray);    */    
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Enter array dimensions: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
