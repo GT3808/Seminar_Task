0e17a903bea0d8afc8273b3ce127a01551cfbd50
Liudmila <topal.liudmila89@gmail.com>
2023-02-11 19:18:15 +0200

    Task 67

---
+++ Task_67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 67
+
+/*    Напишите программу, которая 
+     будет принимать на вход число и 
+     возвращать сумму его цифр.
+
+     453 -> 12
+     45 -> 9                */
+
+int f(int n)                          // делаем тоже через рекурсию
+{
+if (n < 10)                         // ex: f(123)-> f(12)+3-> f(1)+2 -> 1
+return n;                          // 1 e mai mic ca 10, da,de asta ramine 1
+return f(n / 10) + n % 10;        // si 1 + 2 + 3 = 6 ; rezultatu e 6
+}
+
+
+Console.Clear();
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
